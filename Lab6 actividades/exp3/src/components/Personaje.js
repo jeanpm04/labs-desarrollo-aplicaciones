9ef782d83c0b70eaeefb474f83c525391a327a8b
Rocio00006 <111361647+Rocio00006@users.noreply.github.com>
2024-10-09 01:51:09 -0500

    Exp 3 a

---
+++ Lab6 actividades/exp3/src/components/Personaje.js
@@ -0,0 +1,13 @@
+
+
+function Personaje(){
+    return(
+        <div className="Contenedor">
+            <h3>Perssonajes random</h3>
+            <img className="image" src="https://i.imgur.com/QIrZWGIs.jpg" alt="Alan L. Hart"
+            />
+        </div>
+    )
+}
+
+export default Personaje;
---
+++ Lab6 actividades/exp3/src/components/Tarjeta.js
@@ -0,0 +1,12 @@
+import "./Tarjeta.css"
+
+function Tarjeta(){
+    return (
+        <div className="Tarjeta">
+            <h2>Ejemplo de componente Tarjeta</h2>
+            <p>descripción del componente</p>
+        </div>
+    )
+}
+
+export default Tarjeta;
